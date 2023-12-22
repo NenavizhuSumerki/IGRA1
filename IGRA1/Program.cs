01e1bf6c3995f50c1cf712cebeb835955f7a5eee
NenavizhuSumerki <146851676+NenavizhuSumerki@users.noreply.github.com>
2023-12-22 15:17:51 +0300

    Update Program.cs

--- IGRA1/Program.cs
+++ IGRA1/Program.cs
@@ -1,8 +1,8 @@
-﻿using IGRA;
+using IGRA;
 using System.Threading.Channels;
 
 
-
+Console.WriteLine("Добро пожаловать в игру");
 Console.WriteLine("Введите число игроков:");
 int kolich_igrokov = int.Parse(Console.ReadLine());
 
@@ -16,14 +16,14 @@ for (int i = 0; i < player.Length; i++)
 Random random = new Random();
 
 List<string> names = new List<string> {
-"Алекс Миска","Ярык","Юра Удалялкин","Стриканьоли","Донато Каркасс", "Леди Гага", "Стэн Кэрил", "Жозефина", "Золотая рыбка", "Билли Миллиган", "Евгений Понасенков", "Саня-прост-Саня", "Мамадзибрезов", "Мама Серёжи", "Сыбыр Кирилльский","Билли Джин","Хёнджин"
+"Алекс Миска","Ярык Программист","Юра Удалялкин","Стриканьоли","Донато Каркасс", "Леди Гага", "Стэн Кэрил", "Жозефина", "Золотая рыбка", "Билли Миллиган", "Евгений Понасенков", "Саня-прост-Саня", "Мамадзибрезов", "Мама Серёжи", "Сыбыр Кирилльский","Билли Джин","Хёнджин"
 };
 
 
 for (int i = 0; i < player.Length; i++)
 {
     bool friend = false;
-    int ForRandomName = random.Next(0, 8);
+    int NameRand = random.Next(0, 16);
     int ForRandomFriend = random.Next(0, 2);
     if (ForRandomFriend == 0)
     {
@@ -33,13 +33,13 @@ for (int i = 0; i < player.Length; i++)
     int lives = 100;
 
 
-    player[i].Info(names[ForRandomName], random.Next(1, 100), random.Next(1, 100), friend, lives, random.Next(10, 20));
+    player[i].Info(names[NameRand], random.Next(1, 100), random.Next(1, 100), friend, lives, random.Next(10, 20));
 }
 Console.Clear();
 for (int i = 0; i < player.Length; i++)
 {
     Console.Write(i + 1 + " ");
-    player[i].Print();
+    player[i].Out_tablo();
 }
 
 Console.Write("Выбери своего героя: ");
@@ -50,46 +50,43 @@ int geroj = int.Parse(Console.ReadLine()) - 1;
 
 while (true)
 {
-    Console.WriteLine("\n0 - Информация о всех героях.\n1 - Поменять героя.\n2 - Движение.\n3 - УНИЧТОЖЕНИЕ.\n4 - Битва.\n5 - Лечение.\n6 - Полное восстановление здоровья.\n7 - Смена лагеря.\n8 - Информация о соём герое.");
+    Console.WriteLine("\n0 - Информация о всех героях \n1 - Поменять героя \n2 - Движение \n3 - Убийство \n");
+    Console.WriteLine("4 - Битва \n5 - Лечение \n6 - Полное восстановление здоровья \n7 - Смена лагеря \n8 - Информация о соём герое.\n ");
+    
     Console.Write("Ввод: ");
     int dejstvie = int.Parse(Console.ReadLine());
     Console.WriteLine();
 
 
-    int xp = player[geroj].getxp();
+    int xp = player[geroj].VozvratXP();
     if (xp < 1)
     {
         Console.Clear();
         for (int i = 0; i < player.Length; i++)
         {
             Console.Write(i + 1 + " ");
-            player[i].Print();
+            player[i].Out_tablo();
         }
         Console.WriteLine("Ваш герой отправился на фонтан, давай меняй героя");
         Console.Write("Выбери нового героя: ");
         geroj = int.Parse(Console.ReadLine()) - 1;
     }
 
-    if (dejstvie == 8)
-    {
-        Console.Clear();
-        player[geroj].Print();
-        Console.WriteLine();
-    }
+  
 
     if (dejstvie == 0)
     {
         Console.Clear();
         for (int i = 0; i < player.Length; i++)
         {
-            player[i].Print();
+            player[i].Out_tablo();
         }
     }
 
     if (dejstvie == 1)
     {
         Console.Clear();
-        Console.WriteLine("Выбери своего героя: ");
+        Console.WriteLine("Введи айди персонажа, за которого будешь играть: ");
         geroj = int.Parse(Console.ReadLine()) - 1;
     }
 
@@ -99,40 +96,44 @@ while (true)
         for (int i = 0; i < player.Length; i++)
         {
             Console.Write(i + 1 + " ");
-            player[i].Print();
+            player[i].Out_tablo();
         }
-        Console.WriteLine("Перемещение героя.");
+        Console.WriteLine("Переход на другую координату");
         Console.Write("Введи координаты x: ");
         int x = int.Parse(Console.ReadLine());
         Console.Write("Введи координаты y: ");
         int y = int.Parse(Console.ReadLine());
-        player[geroj].MoveX(x);
+        player[geroj].MooveX(x);
         player[geroj].MoveY(y);
     }
 
+
+    //в случае нападения
     if (dejstvie == 3)
     {
 
         Console.Clear();
-        Console.WriteLine("Режим турбо пушки");
+
+
+       
 
         for (int i = 0; i < player.Length; i++)
         {
             Console.Write(i + 1 + " ");
-            player[i].Print();
+            player[i].Out_tablo();
         }
 
         Console.WriteLine("Введи номер игрока, который будет нашей целью: ");
-        int anigilate = int.Parse(Console.ReadLine()) - 1;
+        int died = int.Parse(Console.ReadLine()) - 1;
 
-        if (player[geroj].getl() == player[anigilate].getl())
+        if (player[geroj].VozvratLager() == player[died].VozvratLager())
         {
             Console.WriteLine("Ты на своих напал\n");
         }
 
-        else if (player[geroj].getx() == player[anigilate].getx() && player[geroj].gety() == player[anigilate].gety())
+        else if (player[geroj].VozvratX() == player[died].VozvratX() && player[geroj].VozvratY() == player[died].VozvratY())
         {
-            player[anigilate].Ubit(anigilate);
+            player[died].Ubit(died);
 
 
         }
@@ -144,46 +145,57 @@ while (true)
 
     }
     if (dejstvie == 4)
-        
+
     {
         Console.Clear();
         for (int i = 0; i < player.Length; i++)
         {
-            Console.Write(i + 1 + " ");
-            player[i].Print();
+            Console.Write($"{i + 1}  "); player[i].Out_tablo();
         }
+
+
+
         Console.WriteLine("Введи номер игрока с которым хотим пообщаться: ");
         int anigilate = int.Parse(Console.ReadLine()) - 1;
 
-        if (player[geroj].getl() == player[anigilate].getl())
+        if (player[geroj].VozvratLager() == player[anigilate].VozvratLager())
         {
-            Console.WriteLine("ЭЭЭ, СВОИ СВОИ БРАТ. Ты волыну то убери.\n");
+            Console.WriteLine("Ты напал на своих. Не делай болье так :(  \n");
         }
 
-        else if (player[geroj].getx() == player[anigilate].getx() && player[geroj].gety() == player[anigilate].gety())
+
+
+
+        else if (player[geroj].VozvratX() == player[anigilate].VozvratX() && player[geroj].VozvratY() == player[anigilate].VozvratY())
         {
+            Console.WriteLine("Ты ввёл правилную координату и переместился к игроку, которого попытался уничтожить! ");
+
+
             int du = player[geroj].getdu();
+
             player[anigilate].Uron(du);
 
             du = player[anigilate].getdu();
+
             player[geroj].Uron(du);
 
-            player[geroj].Print();
-            player[anigilate].Print();
+            player[geroj].Out_tablo();
+
+            player[anigilate].Out_tablo();
 
         }
-        else if (player[geroj].getx() != player[anigilate].getx() && player[geroj].gety() == player[anigilate].gety())
+        else if (player[geroj].VozvratX() != player[anigilate].VozvratX() || player[geroj].VozvratY() != player[anigilate].VozvratY())
         {
-            Console.WriteLine("Дружок - перожок, тобой выбранна неправильная координата. Клуб кожевенного мастерства парой координат дальше.");
+            Console.WriteLine("Выбрана неверная координарта");
         }
 
     }
 
     if (dejstvie == 5)
     {
-        Console.Write("Лечение.");
-        int du = random.Next(5, 20);
-        player[geroj].Doc(du);
+        Console.Write("Персонаж вылечен на 10 хр");
+        int lech = 10;
+        player[geroj].Lechenie(lech);
     }
 
     if (dejstvie == 6)
@@ -197,8 +209,14 @@ while (true)
         Console.WriteLine("Смена лагеря.");
         player[geroj].Lager();
     }
+    if (dejstvie == 8)
+    {
+        Console.Clear();
+        player[geroj].Out_tablo();
+        Console.WriteLine();
+    }
 
-   
+
 
 
 
